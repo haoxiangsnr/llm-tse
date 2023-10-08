33e32912ee5c7b3c86529043fe8d1522605944ad
haoxiangsnr <haoxiangsnr@gmail.com>
2023-10-08 10:36:28 +0800

    optim index page

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -80,6 +80,9 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        disableSwitch: true,
+      }
     }),
 };
 
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -4,24 +4,32 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Demo from '@site/src/components/Demo';
-import { DatePicker, Divider } from 'antd';
-import { Button, Space } from 'antd';
+import { DatePicker, Divider, Row } from 'antd';
+import { Button, Space, Typography } from 'antd';
+
 import styles from './index.module.css';
 import { FilePdfOutlined, GithubOutlined } from '@ant-design/icons';
 
-function HomepageHeader() {
+const { Title, Text } = Typography;
+
+function Banner() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <Divider></Divider>
+    <Typography className={styles.banner}>
+      <Title level={2} className={styles.title}>{siteConfig.title}</Title>
+      <Title level={3} className={styles.title}>
+        Xiang Hao<sup>1,2</sup>, Jibin Wu<sup>1,*</sup>, Jianwei Yu<sup>2</sup>, Chenglin Xu<sup>1</sup>, and Kay Chen Tan<sup>1</sup>
+      </Title>
+      <Title level={5} className={styles.title}>
+        <sup>1</sup>The Hong Kong Polytechnic University, <sup>2</sup>Tencent AI Lab
+      </Title>
+      <Divider />
+      <Row justify="center" align="middle" className={styles.links}>
         <Button type="default" icon={<FilePdfOutlined />}>Paper</Button>
         <Divider type="vertical" />
         <Button type="default" icon={<GithubOutlined />}>Github</Button>
-      </div>
-    </header>
+      </Row>
+    </Typography>
   );
 }
 
@@ -31,10 +39,9 @@ export default function Home() {
     <Layout
       title={`${siteConfig.projectName}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <Demo />
-      </main>
+      <Banner />
+      <Divider />
+      <Demo />
     </Layout>
   );
 }
